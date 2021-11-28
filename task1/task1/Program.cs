fbcd7f60d206def6471ac5eb8c152d69a52efea6
Anatoly Sadowsky <sadowsky83@gmail.com>
2021-11-28 18:11:51 +0700

    Добавьте файлы проекта.

---
+++ task1/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        // Метод. Площади треугольника
+        static void Main(string[] args)
+        {
+           
+            double firstTriangleArea = calcArea(1);
+            double secondTriangleArea = calcArea(2);
+
+            if(firstTriangleArea > secondTriangleArea)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+                Console.ReadKey();
+            }
+            if (firstTriangleArea < secondTriangleArea)
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+                Console.ReadKey();
+            }
+            if (firstTriangleArea == secondTriangleArea)
+            {
+                Console.WriteLine("Площади треугольков равны");
+                Console.ReadKey();
+            }
+        }
+        static double calcArea(int triangle)
+        {
+            Console.WriteLine("Укажите длинну 1 стороны " + triangle + " треугольника (целое число)");
+            int lA1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите длинну 2 стороны " + triangle + " треугольника (целое число)");
+            int lB1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите длинну 3 стороны " + triangle + " треугольника (целое число)");
+            int lC1 = Convert.ToInt32(Console.ReadLine());
+
+            double pPer = (lA1 + lB1 + lC1) / 2; // Полупериметр
+            double tArea = Math.Sqrt(pPer / 2 * (pPer - lA1) * (pPer - lB1) * (pPer - lC1));  // Вычисление площади по формуле Герона 
+
+            return (tArea);
+        }
+
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        // Метод. Объем и площадь поверхности куба
+        static void Main(string[] args)
+        {   
+            cubeCalculation();            
+        }
+
+        static void cubeCalculation()
+        {
+            Console.WriteLine("Введите длинну ребра куба (целое число)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double cubeVolume = Math.Pow(a, 3);
+            double cubeSurfaceArea = Math.Pow(a, 2) * 6;
+
+            Console.WriteLine("Объем куба " + cubeVolume);
+            Console.WriteLine("Площадь поверхности куба " + cubeSurfaceArea);
+            Console.ReadKey();
+        }
+    }
+}
